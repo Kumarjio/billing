79398945065c1e38185d6698695e669e381a810e
selestin <selestinthomas@gmail.com>
2018-05-04 17:22:28 +0530

    create databse

--- connection.php
+++ connection.php
@@ -9,6 +9,22 @@ class Connection {
         try {
 
             $this->DBH = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->user, $this->pass);
+			
+			$STH = $this->DBH->prepare("CREATE TABLE IF NOT EXISTS `invoice` (
+								  `id` int(11) NOT NULL,
+								  `type` varchar(11) NOT NULL,
+								  `notes` text NOT NULL,
+								  `date` varchar(11) NOT NULL,
+								  `duedate` varchar(11) NOT NULL,
+								  `number` int(11) NOT NULL,
+								  `status` int(11) NOT NULL,
+								  `subtotal` double NOT NULL,
+								  `discount` double NOT NULL,
+								  `shipping` varchar(100) NOT NULL,
+								  `gst` double(25,0) NOT NULL,
+								  `total` double NOT NULL
+								) ENGINE=InnoDB DEFAULT CHARSET=latin1;");
+			$STH->execute();
         }
         catch (PDOException $e) {
 
@@ -19,4 +35,5 @@ class Connection {
 
         $this->DBH = null;
     }
+	
 }
