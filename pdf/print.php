9ff47f3baecec5ee4e7f4dd7e04fce3adf8c0cad
selestin <selestinthomas@gmail.com>
2018-05-10 23:37:07 +0530

    pdf

--- pdf/print.php
+++ pdf/print.php
@@ -10,7 +10,7 @@
 
 				$cell_width = 45;
 				$pdf->MultiCell($cell_width, 5, "BASIL ASSOCIATES, TAX PRACTITIONER, CITY ESTATE, M C ROAD, PERUMBABOOVR", 0, Alignment.LEFT, false);
-				$pdf->SetXY($current_x + 150, $current_y);
+				$pdf->SetXY($current_x + 400, $current_y);
 				
 				$cell_width = 100;
 				$current_x = $pdf->GetX();
@@ -32,7 +32,7 @@
 				$html .= '<table border="1" width="80%" align="center">
 								<tr><td height="10" colspan="9"></td></tr>
 								<tr>
-									<td bgcolor="#CCCCCC" width="800" height="30" colspan="9"><b>Project Manager: '.$name.'</b></td>
+									<td bgcolor="#CCCCCC" width="800" height="50" colspan="9"><b>Bill Details: '.$name.'</b></td>
 								</tr>';
 					$html .= '<tr>
 									<td bgcolor="#FFFFFF" width="40" height="30"><b>#</b></td>
@@ -41,15 +41,27 @@
 									<td bgcolor="#FFFFFF" align="center" width="80" height="30"><b>Job No</b></td>
 									<td bgcolor="#FFFFFF" align="center" width="170" height="30"><b>Customer</b></td>
 									<td bgcolor="#FFFFFF" align="center" width="230" height="30"><b>Address</b></td>
-									<td bgcolor="#FFFFFF" align="center" width="190" height="30"><b>Insurance Company</b></td>
-									<td bgcolor="#FFFFFF" align="center" width="100" height="30"><b>Loss Type</b></td>
-									<td bgcolor="#FFFFFF" align="center" width="100" height="30"><b>Ref. Source</b></td>
-								</tr></table>';
+									
+								</tr>
+
+								<tr>
+									<td bgcolor="#FFFFFF" width="40" height="30"><b>#</b></td>
+									<td bgcolor="#FFFFFF" width="110" height="30"><b>Entered Date</b></td>
+									<td bgcolor="#FFFFFF" align="center" width="120" height="30"><b>Est. Uploaded</b></td>
+									<td bgcolor="#FFFFFF" align="center" width="80" height="30"><b>Job No</b></td>
+									<td bgcolor="#FFFFFF" align="center" width="170" height="30"><b>Customer</b></td>
+									<td bgcolor="#FFFFFF" align="center" width="230" height="30"><b>Address</b></td>
+									
+								</tr>
+
+								</table>';
+				echo $html;
+				exit;					
 				$pdf->WriteHTML($html);
 				$filename = time();
 				unlink("pdf.pdf");
 				$pdf->Output("".$filename.".pdf","F");	
-				header("Location:pdf.pdf");			
+				header("Location:".$filename.".pdf");			
 				exit;
 
 ?>
